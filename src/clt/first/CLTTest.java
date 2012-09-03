df2fb7eb5f6350e0226b0b53e2569a96dbb747aa
jazzking <kitaek.kim@gmail.com>
2012-09-03 16:47:32 +0900

    4th. Commit
    Signed-off-by: jazzking <kitaek.kim@gmail.com>

--- src/clt/first/CLTTest.java
+++ src/clt/first/CLTTest.java
@@ -5,5 +5,6 @@ public class CLTTest {
 	    System.out.println("CLT Test!");
 	    System.out.println("CLT Test!_0903");
 	    System.out.println("CLT Test!_0903_2nd");
+	    System.out.println("CLT Test!_0903_4th");
 	  }
 }

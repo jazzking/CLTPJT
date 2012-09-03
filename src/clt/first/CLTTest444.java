b7bb59b4187800ef0e5c043f4db3fedb466663f1
jazzking <kitaek.kim@gmail.com>
2012-09-03 17:11:10 +0900

    4444 commit

---
+++ src/clt/first/CLTTest444.java
@@ -0,0 +1,10 @@
+package clt.first;
+
+public class CLTTest444 {
+	public static void main(String[] args) {
+	    System.out.println("CLT Test!");
+	    System.out.println("CLT Test!_0903");
+	    System.out.println("CLT Test!_0903_2nd");
+	    System.out.println("CLT Test!_0903_4th");
+	  }
+}
